211e88dc48d42eff180414f379afa00fa33a94c0
David <david.berchtold@gmx.de>
2024-09-08 15:10:09 +0200

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,13 @@
 <template>
-  <Header></Header>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
   <router-view />
 </template>
 
-<script>
-import Header from '@/components/Header.vue'
-export default {
-  components: {
-    Header
-  }
-}
-</script>
+
 
 <style>
-body {
-  margin: 0;
-  font-family: 'Open Sans', sans-serif;
-}
 
-input {
-  border: 0;
-  border-radius: 8px;
-  width: 100%;
-  height: 30px;
-}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,13 @@
 <template>
-  <RecipeItem 
-  :image="recipe.recipe.image"
-  :label="recipe.recipe.label"
-  :attributes="recipe.recipe.dietLabels.join(', ')"
-  :url="recipe.recipe.url"
-   v-for="recipe in recipes" />
+  <div class="home">
+    <h1>This is an home page</h1>
+  </div>
 </template>
 
 <script>
-import RecipeItem from '@/components/RecipeItem.vue';
 export default {
   components: {
-    RecipeItem
-  },
-  data() {
-    return {
-      recipes: []
-    }
-  },
-  methods: {
-    async getRecipes() {
-      const query = 'curry';
-      const appId = 'd7212edc';
-      const appKey = '3515d1c2238911f0dde38cb661b1f67e';
-      const url = `https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}`;
-      let resp = await fetch(url);
-      this.recipes = (await resp.json()).hits;
-      console.log(this.recipes);
-    }
-  },
-  mounted(){
-    this.getRecipes();
+    
   }
 }
 </script>
